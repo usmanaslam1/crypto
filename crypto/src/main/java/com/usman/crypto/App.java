b03cbdeb9e7fefbe05905660c0cb5dd0a859abbe
Usman Aslam <Usman Aslam>
2022-03-22 14:26:52 -0700

    initial commit

---
+++ crypto/src/main/java/com/usman/crypto/App.java
@@ -0,0 +1,13 @@
+package com.usman.crypto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
